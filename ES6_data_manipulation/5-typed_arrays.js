1f8e602036c2296bc60e30a688e3bc4c5661e0d1
elshadyusifov <e.yusifov335@gmail.com>
2025-08-27 20:36:57 +0400

    int8 array

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const int8 = new Int8Array(length);
+  int8[position] = value;
+  return int8;
+}
