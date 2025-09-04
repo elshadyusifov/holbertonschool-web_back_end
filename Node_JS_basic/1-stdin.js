09e3214287404cb3e873f818b617b8e03f4cc736
elshadyusifov <e.yusifov335@gmail.com>
2025-09-04 18:21:02 +0400

    changesss

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,16 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-let input = '';
-
-process.stdin.on('data', (chunk) => {
-  input += chunk;
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
-  const name = input.trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  }
   process.stdout.write('This important software is now closing\n');
 });
