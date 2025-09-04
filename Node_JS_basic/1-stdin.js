20f35e0432f7a2159ce24bea6778cf931a98fe30
elshadyusifov <e.yusifov335@gmail.com>
2025-09-04 18:17:15 +0400

    change code trim()

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,12 +2,16 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r`);
+let input = '';
+
+process.stdin.on('data', (chunk) => {
+  input += chunk;
 });
 
-// When the input stream ends (CTRL+D), show closing message
 process.stdin.on('end', () => {
+  const name = input.trim();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
   process.stdout.write('This important software is now closing\n');
 });
