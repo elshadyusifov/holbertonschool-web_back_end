04b52a075673aba753b174148999011c0fa8f3a8
elshadyusifov <e.yusifov335@gmail.com>
2025-09-07 17:23:54 +0400

    http import  async

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2]; // fayl adı argumentdən götürülür
+
+const app = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
+
+  if (pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (pathname === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    countStudents(database)
+      .then((output) => {
+        res.end(output);
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
