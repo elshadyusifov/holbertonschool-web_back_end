08b7c81b47557f361b4d60107d62435ee90dbf11
elshadyusifov <e.yusifov335@gmail.com>
2025-09-07 18:14:58 +0400

    studentcontroller

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,47 @@
+import { readDatabase } from '../utils.js';
+
+class StudentsController {
+  /**
+   * GET /students
+   */
+  static async getAllStudents(req, res) {
+    const dbFile = process.argv[2];
+
+    try {
+      const data = await readDatabase(dbFile);
+      const fields = Object.keys(data).sort((a, b) =>
+        a.toLowerCase().localeCompare(b.toLowerCase())
+      );
+
+      let responseText = 'This is the list of our students\n';
+      fields.forEach((field) => {
+        responseText += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
+      });
+      res.status(200).send(responseText.trim());
+    } catch (err) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  /**
+   * GET /students/:major
+   */
+  static async getAllStudentsByMajor(req, res) {
+    const dbFile = process.argv[2];
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      return res.status(500).send('Major parameter must be CS or SWE');
+    }
+
+    try {
+      const data = await readDatabase(dbFile);
+      if (!data[major]) data[major] = [];
+      res.status(200).send(`List: ${data[major].join(', ')}`);
+    } catch (err) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+}
+
+export default StudentsController;
