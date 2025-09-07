7a3b0eac513945812b73f0dcea0721fb6b54d3ec
elshadyusifov <e.yusifov335@gmail.com>
2025-09-07 17:45:54 +0400

    http express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const dbFile = process.argv[2]; // database.csv fayl adı
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
+
+  countStudents(dbFile)
+    .then((content) => {
+      res.write(content);
+      res.end();
+    })
+    .catch((err) => {
+      res.write(`This is the list of our students\n${err.message}`);
+      res.end();
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
