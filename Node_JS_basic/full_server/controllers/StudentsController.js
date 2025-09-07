1ab66a5ecac778df9ffcd7f63e1de8a1955b7e29
elshadyusifov <e.yusifov335@gmail.com>
2025-09-07 19:30:28 +0400

    bos

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,7 +7,8 @@ class StudentsController {
       const students = await readDatabase(dbFile);
       let output = 'This is the list of our students\n';
       const fields = Object.keys(students)
-        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));      fields.forEach((field) => {
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        fields.forEach((field) => {
         output += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
       });
       res.status(200).send(output.trim());
